bd4c02bc8b25d67240b9480cd462cadb535dd58a
Republicsandy <sandy180388@gmail.com>
2022-08-23 07:10:21 +0530

    [sandeep] Max number file added in UC_1

---
+++ Find_Max_Num_Using_Generics/Max_Num_Check.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_Max_Num_Using_Generics
+{
+    internal class Max_Num_Check
+    {
+        public static int MaxIntgerNum(int firstValue, int secondValue, int thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("All three values are same!");
+        }
+    }
+}
---
+++ Find_Max_Num_Using_Generics/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Find_Max_Num_Using_Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to MaximumTest using Generics!");
+            int MaximumInt = Max_Num_Check.MaxIntgerNum(30, 20, 10);
+            Console.WriteLine("Maximum integer value is : " + MaximumInt);
+        }
+    }
+}
