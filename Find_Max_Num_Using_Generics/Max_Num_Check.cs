a13664888f194467428a40a39360b4e04ce90ead
Sandeep singh <sandy180388@gmail.com>
2022-08-29 17:44:17 +0530

    [sandeep] float number check file is added in UC_2

--- Find_Max_Num_Using_Generics/Max_Num_Check.cs
+++ Find_Max_Num_Using_Generics/Max_Num_Check.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace Find_Max_Num_Using_Generics
 {
-    internal class Max_Num_Check
+    class Max_Num_Check
     {
-        public static int MaxIntgerNum(int firstValue, int secondValue, int thirdValue)
+        public static int MaximumIntgerNumber(int firstValue, int secondValue, int thirdValue)
         {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("All three values are same!");
+        }
+        public static float MaximumFloatNumber(float firstValue, float secondValue, float thirdValue)
+        {
+            //CompareTo method is used to compare two numbers
             if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
             {
                 return firstValue;
--- Find_Max_Num_Using_Generics/Program.cs
+++ Find_Max_Num_Using_Generics/Program.cs
@@ -2,13 +2,25 @@
 
 namespace Find_Max_Num_Using_Generics
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Welcome to MaximumTest using Generics!");
-            int MaximumInt = Max_Num_Check.MaxIntgerNum(30, 20, 10);
-            Console.WriteLine("Maximum integer value is : " + MaximumInt);
+            Console.WriteLine("To Find MaximumTest using Generics!");
+            int getValueByUser = Convert.ToInt32(Console.ReadLine());
+            switch (getValueByUser)
+            {
+                case 1:
+                    int maximumInteger = Max_Num_Check.MaximumIntgerNumber(30, 20, 10);
+                    Console.WriteLine("Maximum integer value is : " + maximumInteger);
+                    break;
+                    case 2:
+                    float maximumFloatNumber = Max_Num_Check.MaximumFloatNumber(111.2f, 22.45f, 78.56f);
+                    Console.WriteLine("Maximum float value is : " + maximumFloatNumber);
+                    break;
+                default: throw new Exception("invaled Number enterd");
+            }
+           
         }
     }
 }
